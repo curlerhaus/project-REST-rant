552e9fe77082cadaf81d3588d582ba2384f664c3
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-07-21 20:33:16 -0500

    added 404 page route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send(`Hello World!`)
 });
 
+app.get('*', (res, req) => {
+    res.send(`<h1>404 Page</h1>`)
+})
+
 app.listen(process.env.PORT, function () {
     console.log(`Server is running on port ${process.env.PORT}\n
     http://localhost:${process.env.PORT}`);
