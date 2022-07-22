8d79b77e59981b9b7d31da995ce5a45245fc53f8
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-07-21 20:49:24 -0500

    added places controler

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+app.use('/places', require('./contollers/places'))
+
 app.get('/', (req, res) => {
     res.send(`Hello World!`)
 });
 
 app.get('*', (res, req) => {
-    res.send(`<h1>404 Page</h1>`)
+    res.status(404).send(`<h1>404 Page</h1>`)
 })
 
 app.listen(process.env.PORT, function () {
