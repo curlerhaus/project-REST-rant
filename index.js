646187d8af899d70f041b943bdfbebfbe0fb31ea
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-07-25 15:18:51 -0500

    added places view with mock data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 
-app.use('/places', require('./contollers/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -20,3 +20,5 @@ app.listen(process.env.PORT, function () {
     console.log(`Server is running on port ${process.env.PORT}\n
     http://localhost:${process.env.PORT}`);
 })
+
+
