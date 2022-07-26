18daed8d7497e8fe98758a7bcfb17fb98cf586c5
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-07-25 20:01:01 -0500

    adding CSS

--- controllers/places.js
+++ controllers/places.js
@@ -15,6 +15,6 @@ router.get('/', (req, res) => {
         pic: 'http://placekitten.com/250/250'
       }]
           res.render('places/index', { places })
-})
+});
 
 module.exports = router
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
 
 
 app.use('/places', require('./controllers/places'))
