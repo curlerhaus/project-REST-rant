2b3778d2b1687cad353f7325cad0fc97db08ff0c
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-07-26 20:09:30 -0500

    new places alsmost working

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,33 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'CSS BBQ',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Korean BBQ',
-        pic: '/images/bbq-rest.jpg'
-      }, {
-        name: 'Smoking Java',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Southern BBQ',
-        pic: '/images/smoker.jpg'
-      }]
-          res.render('places/index', { places })
+    res.render('places/index', { places })
 });
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+// router.post('/', (req, res) => {
+//     res.send('POST /places')
+//     console.log(req.body)
+// })
+
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
-})
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'CSS BBQ',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Korean BBQ',
+    pic: '/images/bbq-rest.jpg'
+  }, {
+    name: 'Smoking Java',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Southern BBQ',
+    pic: '/images/smoker.jpg'
+  }]
