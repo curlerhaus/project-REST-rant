89afbba669d186087f7c95c3bf3315c67ac9aa5d
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-07-28 20:47:05 -0500

    add delete

--- controllers/places.js
+++ controllers/places.js
@@ -25,6 +25,22 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
 
+//DELETE ROUTE
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+  })
+  
+
 //   SHOW ROUTE
   router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
@@ -35,7 +51,7 @@ router.post('/', (req, res) => {
         res.render('error404')
     }
     else {
-        res.render('places/show', {place: places[id]})
+        res.render('places/show', {place: places[id], id})
 
     }
   })
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,32 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <div class = 'row'>
+                <div class = 'col-sm-6'>
+                    <img src={data.place.pic} alt={data.place.name} />
+                </div>
+                <div class = 'col-sm-6'>
+                    <h1>{data.place.name}</h1>
+                    <h2>Rating</h2>
+                    <h2>Description</h2>
+                    <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}.</p>
+                </div>
+            </div>
+            <div>
+                <h2>Comments</h2>
+                <p>No comments yet.</p>
+            </div>
+            <div>
+                <a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
+            </div>
+            <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                <button type='submit' className='btn btn-danger'>Delete
+                </button>
+            </form>
           </main>
         </Def>
     )
