002e9c5c95eff3943850b5d6e646832bad466822
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-08-07 15:37:52 -0500

    added comment html

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,6 +4,26 @@ const Def = require('../default')
 
 //pasted from class page
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+          No comments yet!
+        </h3>
+      )
+      if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+      }
+  
     return (
         <Def>
   <main>
@@ -36,13 +56,17 @@ function show (data) {
     </div>
     <div>
                  <h2>Comments</h2>
-                 <p>No comments yet.</p>
+                 {comments}
+                 {/* <p>No comments yet.</p> */}
              </div>
              
   </main>
     </Def>
     )
 }
+
+module.exports = show
+
 
 // function show (data) {
 //     return (
@@ -82,34 +106,3 @@ function show (data) {
 
 
 
-module.exports = show
-
-// function show (data) {
-//     return (
-//         <Def>
-//   <main>
-//     <div className="row">
-//       <div className="col-sm-6">
-//         <img src={data.place.pic} alt={data.place.name} />
-//         <h3>
-//           Located in {data.place.city}, {data.place.state}
-//         </h3>
-//       </div>
-//       <div className="col-sm-6">
-//         ...
-//         <h2>
-//           Description
-//         </h2>
-//         <h3>
-//           {data.place.showEstablished()}
-//         </h3>
-//         <h4>
-//           Serving {data.place.cuisines}
-//         </h4>
-//         ...
-//     </div>
-//     </div>
-//   </main>
-//     </Def>
-//     )
-// }
